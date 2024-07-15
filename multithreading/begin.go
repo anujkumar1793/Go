f1f67bf66e7d8aa5aa47d7bcd27c3b8f2c3bca95
Anuj Kumar <anujkumar4200000@gmail.com>
2024-07-15 10:43:30 +0530

    Update begin.go

--- multithreading/begin.go
+++ multithreading/begin.go
@@ -1,1 +1,26 @@
+//Многопоточность - это создание нескольких процессов, выполняющихся независимо друг от друга.
 
+package main
+
+import (
+    "fmt"
+    "time"
+    )
+
+func out(from, to int, ch chan bool) {
+    for i:=from; i<=to; i++ {
+        time.Sleep(50 * time.Millisecond)
+        fmt.Println(i)
+    }
+    ch <- true
+}
+
+func main() {
+    ch := make(chan bool)
+
+    go out(0, 5, ch)
+    go out(6, 10, ch)
+
+    <-ch
+    <-ch
+}
